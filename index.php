8601162babb1e1ebacab06f1c7b2159cba48bfab
@jaredscribe <jaredscribe@zoho.com>
2022-09-14 21:20:42 -0700

    List of tools, tutorials, articles, datasets, etc

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Vinegar Bros Analytics</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="">
+ 
+  <link rel="stylesheet" href="/static/lib/bootstrap-4.5.3-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/static/styles.css">
+
+  <!-- add after bootstrap.min.css -->
+
+  <script src="static/lib/jquery-3.5.1.min.js"></script>
+  <script src="static/lib/bootstrap-4.5.3-dist/js/bootstrap.bundle.min.js"></script>
+
+  <link type="text/plain" rel="author" href="humans.txt">
+ 
+</head>
+<body>
+
+<div class="container-fluid">
+
+<h2>Tools (and Tutorials)</h2>
+<ul>
+    <li>Bash shell.   Zsh</li>
+    <li>Vim    (Vimtutor)</li>
+    <li>Visualization and modeling - D3js.org - <a href="https://observablehq.com/tutorials">D3.js tutorials at ObservableHQ</a></li>
+    <li>Javascript - functional programming paradigm.</li>
+    <li>Python3: IronPython shell, Scipy, Numpy</li>
+    <li><a href="https://www.coursera.org/learn/learn-to-program">Learn to Program (U of Toronto, Coursera, ~25 hrs)</a> Based on the <a href="https://developers.google.com/edu/python/introduction?hl=en">Google Python course</a></li>)
+    <li><a href="https://www.coursera.org/collections/machine-learning">Andrew Ng's Machine Learning Collection</a></li> 
+    {{expand section}}
+</ul>
+
+<h2>Datasets to analyze</h2>
+<ul>
+    <li><a href="http://data.gov">Data.gov</a></li>
+    <li><a href="https://meta.wikimedia.org/wiki/Datasets">Wikipedia and meta.wikimedia Datasets</a></li>
+    <li>Sefaria Datasets</li>
+    {{expand section}}
+</ul>
+
+<h2>Scientific questions to answer</h2>
+<ul>
+    <li>Is the population of honeybees in the Willamette Valley region, increasing, decreasing, or remaining static, and what is the cause of the change?  What other population statistics help us analyze their health and vibrancy?</li>
+    <li>What is the purpose of animal life?  of plant life?</li>
+    <li>Is there a purpose to human life as distinct from the lives of brute animals?  Or is life merely "what we make of it"?</li>
+    <li>Did life originate an evolutionary process of "random mutation"?  Or is there a design toward which life is evolving?  Is there an intelligence to the design?</li>
+{{expand section}}
+</ul>
+
+<h2>Philosophy of Probabilistic reasoning, Causality, & Data science</li>
+<ul>
+    <li>Wolfram, Stephen "A New Kind of Science".  WolframAlpha</li>
+    <li>Pearl, Judaea "The Book of Why"</li>
+    <li>Barrow, John <a href="https://www.amazon.com/Infinite-Book-Boundless-Timeless-Endless/dp/1400032245/ref=sr_1_4?crid=FCWH6DI67NEX&keywords=the+infinite+book&qid=1663212695&sprefix=the+infinite+book%2Caps%2C159&sr=8-4">A Brief Introduction to the Infinite Book</a></li>
+</ul>
+
+<h2>Articles on Data Analysis and Science</h2>
+<ul>
+    <li><a href="https://en.wikipedia.org/wiki/Data_analysis">wiki/Data_analysis</a></li>
+    <li><a href="https://en.wikipedia.org/wiki/Data_wrangling">wiki/Data Wrangling or Munging</a></li>
+    <li><a href="https://en.m.wikiversity.org/wiki/Data_analysis">wikiversity/Data_analysis</a></li>
+</ul>
+
+<blockquote>
+"The telos is first in order of intention, last in order of action" &mdash; Aristotle
+</blockquote>
+
+</div> <!-- end .container -->
+<footer class="row">
+
+    <hr />
+    <p class="float-right"><a href="#">Back to top</a></p>
+
+    <nav class="navbar col-sm">
+  
+	<p><?php echo "last modified: " .date ("F d Y H:i:s.", filemtime("header.shtml")); ?> </li></p>
+
+  </nav>
+  </footer>
+
+</body>
+</html>
