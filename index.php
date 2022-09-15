fe9ccfebd8e92ba2f9b4c6a7bb1fd241c20843c3
@jaredscribe <jaredscribe@zoho.com>
2022-09-15 07:35:41 -0700

    Added many links, #Ethics section

--- index.php
+++ index.php
@@ -26,9 +26,10 @@
 
 <h2>Tools (and Tutorials)</h2>
 <ul>
-    <li>Bash shell.   Zsh</li>
+    <li>Bash shell. <a href="https://duckduckgo.com/?t=lm&q=top+10+bash+commands&ia=web">Top 10 shell commands</a>, shellcheck, devhints.io/bash. <a href="https://en.wikipedia.org/wiki/Unix_philosophy#Do_One_Thing_and_Do_It_Well">The Unix Philosophy - Do One Thing and Do it Well</a>.  Zsh, <a href="https://ohmyz.sh/">ohmyz.sh</a></li>
     <li>Vim    (Vimtutor)</li>
-    <li>Visualization and modeling - D3js.org - <a href="https://observablehq.com/tutorials">D3.js tutorials at ObservableHQ</a></li>
+    <li>Visualization and modeling <a href="http://D3js.org">D3js.org</a> &mdash; <a href="https://observablehq.com/tutorials">D3.js tutorials at ObservableHQ</a></li>
+    <li>Git, <a href="https://github.com/skills/introduction-to-github">Intro to Github (1hr)</a>, <a href="https://docs.github.com/en/get-started/quickstart/hello-world">Git Hello World</a>, <a href="https://training.github.com/downloads/github-git-cheat-sheet/">Git Cheat sheet</a></li>
     <li>Javascript - functional programming paradigm.</li>
     <li>Python3: IronPython shell, Scipy, Numpy</li>
     <li><a href="https://www.coursera.org/learn/learn-to-program">Learn to Program (U of Toronto, Coursera, ~25 hrs)</a> Based on the <a href="https://developers.google.com/edu/python/introduction?hl=en">Google Python course</a></li>)
@@ -39,8 +40,9 @@
 <h2>Datasets to analyze</h2>
 <ul>
     <li><a href="http://data.gov">Data.gov</a></li>
-    <li><a href="https://meta.wikimedia.org/wiki/Datasets">Wikipedia and meta.wikimedia Datasets</a></li>
-    <li>Sefaria Datasets</li>
+    <li><a href="https://meta.wikimedia.org/wiki/Datasets">Wikipedia and meta.wikimedia Datasets</a>. more <a href="https://www.tensorflow.org/datasets/catalog/wikipedia">wikipedia datasets on tensorflow</a></li>
+    <li><a href="https://www.sefaria.org/visualizations">Sefaria Visualizations</a></li>
+    <li><a href="https://www.tableau.com/learn/articles/free-public-data-sets">Free public Datasets on Tableau</a></li>
     {{expand section}}
 </ul>
 
@@ -53,11 +55,21 @@
 {{expand section}}
 </ul>
 
-<h2>Philosophy of Probabilistic reasoning, Causality, & Data science</li>
+<h2>Philosophy of Probabilistic reasoning, Causality, & Data science</h2>
 <ul>
     <li>Wolfram, Stephen "A New Kind of Science".  WolframAlpha</li>
     <li>Pearl, Judaea "The Book of Why"</li>
-    <li>Barrow, John <a href="https://www.amazon.com/Infinite-Book-Boundless-Timeless-Endless/dp/1400032245/ref=sr_1_4?crid=FCWH6DI67NEX&keywords=the+infinite+book&qid=1663212695&sprefix=the+infinite+book%2Caps%2C159&sr=8-4">A Brief Introduction to the Infinite Book</a></li>
+    <li>Barrow, John <a href="https://www.amazon.com/Infinite-Book-Boundless-Timeless-Endless/dp/1400032245/ref=sr_1_4?crid=FCWH6DI67NEX&keywords=the+infinite+book&qid=1663212695&sprefix=the+infinite+book%2Caps%2C159&sr=8-4">The Infinite Book</a></li>
+</ul>
+
+<h2>Ethics and Philosophy</h2>
+<ul>
+    <li><a href="http://jaredscribe.com/internet-ethics"</a>jaredscribe.com/internet-ethics</a>
+    <li><a href="">Richard Stallman explains the Free Software philosophy of Four Freedoms</a>
+    <li>GNU is Not Unix, Linux Kernel</a>
+    <li>FSF &mdash; Free Software Foundation, The GNU Public License (GPL)</li>
+    <li>The Open Source Movement and "permissive licenses"</li>
+    <li>EFF &mdash; Electronic Frontier Foundation</li>
 </ul>
 
 <h2>Articles on Data Analysis and Science</h2>
