72f9cad7d88b3f8342799f0df062b8e6a374bc98
@jaredscribe <jaredscribe@zoho.com>
2022-09-19 14:34:02 -0700

    VSCodium, Electron, and many more links

--- index.php
+++ index.php
@@ -26,12 +26,13 @@
 
 <h2>Tools (and Tutorials)</h2>
 <ul>
-    <li>Bash shell. <a href="https://duckduckgo.com/?t=lm&q=top+10+bash+commands&ia=web">Top 10 shell commands</a>, shellcheck, <a href="https://devhints.io/bash">devhints.io/bash</a>, <a href="https://learnxinyminutes.com/docs/bash/">Learn Bash in 10 minutes</a>, <a href="https://en.wikipedia.org/wiki/Unix_philosophy#Do_One_Thing_and_Do_It_Well">The Unix Philosophy - Do One Thing and Do it Well</a>.  Zsh, <a href="https://ohmyz.sh/">ohmyz.sh</a></li>
-    <li>Vim text editor    (Vimtutor).  <a href="https://neovim.io/">Neovim.io</a>?</li>
-    <li><b>Git</b> distributed version control system (VCS) for collaboration <a href="https://github.com/skills/introduction-to-github">Intro to Github (1hr)</a>, <a href="https://docs.github.com/en/get-started/quickstart/hello-world">Git Hello World</a>, <a href="https://training.github.com/downloads/github-git-cheat-sheet/">Git Cheat sheet</a>, <a href="https://docs.github.com/en/authentication/connecting-to-github-with-ssh/about-ssh">SSH Public Key Cryptography</a></li>
-    <li>Webpage quickstart, with <a href="https://getbootstrap.com/docs/5.2/getting-started/introduction/">Bootstrap Graphic design patterns</a>, <a href="https://en.wikipedia.org/wiki/HTML5">HTML5</a>, the <a href="https://dom.spec.whatwg.org/#interface-document">Document Object Model (DOM)</a>, Cascading Style Sheets (CSS).</li>
-    <li><b>Visualization and modeling</b> <a href="http://D3js.org">D3js.org</a> uses SVG for rendering, &mdash; <a href="https://observablehq.com/tutorials">D3.js tutorials at ObservableHQ</a></li>
-    <li><b>Javascript/ECMAScript</b>: <a href="https://observablehq.com/@observablehq/learn-javascript-introduction?collection=@observablehq/tutorial">Just enough to be dangerous with D3.js</a>, (eslint) <a href="https://eloquentjavascript.net/">Eloquent Javascript</a>, <a href="https://www.linkedin.com/learning/learning-functional-programming-with-javascript-es6-plus/a-functional-approach-to-transform-code">Learning functional programming ES6 (1.5 hrs)</a>, MapReduce</li>
+    <li>Shell:  bash, zsh. <a href="https://duckduckgo.com/?t=lm&q=top+10+bash+commands&ia=web">Top 10 shell commands</a>, shellcheck, <a href="https://devhints.io/bash">devhints.io/bash</a>, <a href="https://learnxinyminutes.com/docs/bash/">Learn Bash in 10 minutes</a>, <a href="https://en.wikipedia.org/wiki/Unix_philosophy#Do_One_Thing_and_Do_It_Well">The Unix Philosophy - Do One Thing and Do it Well</a>.  Zsh, <a href="https://ohmyz.sh/">ohmyz.sh</a></li>
+    <li>Text editor:  vim (c.f. vimtutor). <a href="https://neovim.io/">Neovim.io</a>?  <a href="https://vscodium.com/">VS Codium</a> (built with Electron Framework)</a> <a href="https://stackoverflow.com/questions/473478/how-to-do-jslint-in-vim#5893447">JSLint/Hint Vim plugin</a>li>
+    <li>VC: <b>Git</b> distributed version control system (VCS) for collaboration <a href="https://github.com/skills/introduction-to-github">Intro to Github (1hr)</a>, <a href="https://docs.github.com/en/get-started/quickstart/hello-world">Git Hello World</a>, <a href="https://training.github.com/downloads/github-git-cheat-sheet/">Git Cheat sheet</a>, <a href="https://docs.github.com/en/authentication/connecting-to-github-with-ssh/about-ssh">Connect with SSH</a>, <a href="https://en.wikipedia.org/wiki/Public-key_cryptography">Public Key Cryptography</a></li>
+    <li>Webpage quickstart: <a href="https://getbootstrap.com/docs/5.2/getting-started/introduction/">Bootstrap HTML/CSS Graphic design patterns</a>, <a href="https://en.wikipedia.org/wiki/HTML5">HTML5</a>, the <a href="https://dom.spec.whatwg.org/#interface-document">Document Object Model (DOM)</a>, Cascading Style Sheets (CSS) => LESS, SCSS </li>
+    <li><b>Visualization and modeling:</b> <a href="http://D3js.org">D3js.org</a> uses SVG for rendering, &mdash; <a href="https://observablehq.com/tutorials">D3.js tutorials at ObservableHQ</a></li>
+    <li><b>Javascript/ECMAScript</b>: <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference">MDN Reference Docs</a><a href="https://observablehq.com/@observablehq/learn-javascript-introduction?collection=@observablehq/tutorial">Just enough to be dangerous with D3.js</a>, <a href="https://eloquentjavascript.net/">Eloquent Javascript</a>, <a href="https://www.linkedin.com/learning/learning-functional-programming-with-javascript-es6-plus/a-functional-approach-to-transform-code">Learning functional programming ES6 (1.5 hrs)</a>, MapReduce, <a href="http://crockford.com/javascript/">D.Crockford:  The wrrrld's most misunderstood programming language</a>, <a href="https://www.howjavascriptworks.com/">How Javscript Works (Crockford, 2018)</a>, <a href="https://tc39.es/ecma262/">ECMAScript Standard and Brief Changelog</a>, <a href="https://www.infoq.com/presentations/Heretical-Open-Source/">A Heretic's Perspective (1hr lecture) (Crockford, 2011)</a></li>
+    <li><a href="https://github.com/electron/electron-quick-start">Electron quickstart</a>, <a href="https://developer.chrome.com/docs/extensions/mv3/getstarted/">Chrome extension quickstart (1-2hrs)</a></li>
     <li><b>RESTful Web Application Architecture: web service API (can use Python Flask, Ruby Sinatra, or Node.js for example), with a frontend framwork based on Model-View-Controller (MVC) design pattern and AJAX & JSON, such as React.js or Angular. <a href="https://www.udemy.com/course/es6-in-depth/?LSNPUBID=0F1O0otUXQc&ranEAID=0F1O0otUXQc&ranMID=39197&ranSiteID=0F1O0otUXQc-zD1ezaxEqNYcW_Ehtx80EQ&utm_medium=udemyads&utm_source=aff-campaign">ES6/Node/React/Webpack (5hrs tutorial)</a></li>
     <li><b>Python</b>3: IronPython shell.  Scipy, Numpy.  Flask (Simple REST web server)</li>
     <li><a href="https://www.coursera.org/learn/learn-to-program">Learn to Program (U of Toronto, Coursera, ~25 hrs)</a> Based on the <a href="https://developers.google.com/edu/python/introduction?hl=en">Google Python course</a></li>)
@@ -98,6 +99,9 @@
     <li> glitch.com, jsfiddle.com, jsbin.com</li>
     <li><a href="https://en.m.wikiversity.org/wiki/Data_analysis">wikiversity/Data_analysis</a></li>
     <li><a href="http://trifacta.com">Trifacta</a> is advised by Mike Bostock, who made D3</li>
+    <li><a href="https://docs.requarks.io/install/ubuntu">Install Wiki.js on Ubuntu with Docker</a></li>
+    <li><a href="https://docs.docker.com/engine/install/ubuntu/">Docker on Ubuntu</a></li>
+    <li><a href="https://js.wiki/">Wiki.js</a> uses Markdown with Git and Postgres</li>
 </ul>
 
 </div> <!-- end .container -->
@@ -108,7 +112,7 @@
 
     <nav class="navbar col-sm">
   
-	<p><?php echo "last modified: " .date ("F d Y H:i:s.", filemtime("header.shtml")); ?> </li></p>
+	<p><?php echo "last modified: " .date ("F d Y H:i:s.", filemtime("index.php")); ?> </li></p>
 
   </nav>
   </footer>
